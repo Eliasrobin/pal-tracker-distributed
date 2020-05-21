1db6b9472ae533ce15c0aa6a5b712fa53056753c
eliasrobin <robin.elias@cognizant.com>
2020-05-21 11:58:02 +0530

    5555

--- components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
+++ components/rest-support/src/main/java/io/pivotal/pal/tracker/restsupport/RestConfig.java
@@ -4,16 +4,17 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.RestTemplate;
 
 
 @Configuration
 public class RestConfig {
 
-//    @Bean
-//    @LoadBalanced
-//    public RestOperations restOperations() {
-//        return new RestTemplate();
-//    }
+    @Bean
+    public RestOperations restOperations() {
+        return new RestTemplate();
+    }
 
     @Bean
     public ObjectMapper objectMapper() {
